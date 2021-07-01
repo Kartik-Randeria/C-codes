7bed7ce7df1e923b92c918296db1c8cc1bc1b3f6
Kartik-Randeria <kartikranderia08@gmail.com>
2021-07-01 18:34:33 +0530

    C# codes initial commit

---
+++ Constructor/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Constructor
+{
+  // Constructor Overloadind : In C# we can overload constructor by creating another constructor with same name and different parameters
+
+  class Program : Parent
+  {
+    public string param1, param2;
+
+    // Static Constructor : It is like static block in java. It will be invoked only once for any number of instances of the class and it's during creation of first instance of the class or the first reference to a static member in the class. Static Constructor is used to initialize static fields. It is used to write the code that needs to be  executed only once. 
+    static Program()
+    {
+      Console.WriteLine("Static Constructor Call");
+    }
+
+    // Default Constructor 
+    Program() : this(2)
+    {
+      param1 = "Welcome";
+      param2 = "Kartik";
+      Console.WriteLine("Default Constructor Call");
+    }
+
+    //Parameterized Constructor : Constructor with atleast one parameter
+    Program(string x, string y)
+    {
+      this.param1 = x;
+      this.param2 = y;
+      Console.WriteLine("Paramterized Constructor Call");
+    }
+
+    // Copy Constructor: It is a Parameterized Constructor that contains a parameter of same class type. It will copy the values from one instance to another instance
+    Program(Program obj)
+    {
+      param1 = obj.param1;
+      param2 = obj.param2;
+      Console.WriteLine("Copy Constructor Call");
+    }
+
+    Program(int i) : base("parent call")
+    {
+      Console.WriteLine("this Inherited Constructor Call");
+    }
+
+    //Destructor
+    ~Program()
+    {
+      Console.WriteLine("Destructor Call");
+    }
+
+
+    static void Main(string[] args)
+    {
+      // first static constructor is invoked
+      Program p1 = new Program(); //Default constructor called automatically
+      Program p2 = new Program("Hello", "World"); //Paramterized constructor called
+      Program p3 = new Program(p2); //Copy constructor called
+
+
+      Console.WriteLine("---------Default Constructor---------");
+      Console.WriteLine(p1.param1 + " " + p1.param2);
+      Console.WriteLine("--------Paramterized Constructor--------");
+      Console.WriteLine(p2.param1 + " " + p2.param2);
+      Console.WriteLine("--------Copy Constructor--------");
+      Console.WriteLine(p3.param1 + " " + p3.param2);
+
+      Console.ReadLine();
+    }
+  }
+
+  class Parent
+  {
+    public Parent()
+    {
+
+    }
+    public Parent(string i)
+    {
+      Console.WriteLine("base Parent Parameterized Constructor Call");
+    }
+  }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Polymorphism
+{
+  // Concepts:
+
+  // Method Overriding: It means redefining same method in child class. In method Overriding, by default base class (parent) method is called.
+
+  // Method Hiding (new keyword): C# supports method hiding. For hiding the base class method from derived class simply declare the derive class method with new keyword
+
+  //virtual and override keyword: In C# for overriding the base class method in derived class, we have to declare base class method as virtual and derived class method as override. If we use override then we don't need to use new keyword to declare method.
+
+  //Mixing Method Overriding and Method Hiding: It is done by using new and virtual keyword since the method of derived class can be virtual and new at the same time. This is required when we want to further override the derived class method into next level, here I am overriding Class B, Test3() method in Class C as shown below :
+  class A
+  {
+    public void Test()
+    {
+      Console.WriteLine("A::Test()");
+    }
+
+    public virtual void Test2()
+    {
+      Console.WriteLine("A::Test2()");
+    }
+    public void Test3()
+    {
+      Console.WriteLine("A::Test3()");
+    }
+  }
+
+  class B : A
+  {
+    // If this overriden method is not declared using new keyword then compiler gives warning as C# supports method hiding
+    public new void Test()
+    {
+      Console.WriteLine("B::Test()");
+    }
+    public override void Test2()
+    {
+      Console.WriteLine("B::Test2()");
+    }
+    public new virtual void Test3()
+    {
+      Console.WriteLine("B::Test3()");
+    }
+  }
+
+  class C : B
+  {
+    public new void Test()
+    {
+      Console.WriteLine("C::Test()");
+    }
+    public override void Test2()
+    {
+      Console.WriteLine("C::Test2()");
+    }
+    public override void Test3()
+    {
+      Console.WriteLine("C::Test3()");
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      A a1 = new A();
+      B b1 = new B();
+      C c1 = new C();
+
+      Console.WriteLine("--------------------------Simple-------------------------------");
+      a1.Test(); //output=> A::Test()
+      b1.Test(); //output=> B::Test()
+      c1.Test(); //output=> C::Test()
+      Console.WriteLine("----------------override and virtual keyword-------------------");
+
+      //If LHS and RHS is different then new keyword hides the subclass method (only PC format allowed Parent-Child)
+      a1 = new B();
+      a1.Test2(); //output=> B::Test2()
+
+      b1 = new C();
+      b1.Test2(); //output=> C::Test2()
+
+      Console.WriteLine("--------------Mixing Overriding and Hiding----------------------");
+
+      a1 = new B();
+      a1.Test3(); //output=> A::Test3()
+
+      a1 = new C();
+      a1.Test3(); //output=> A::Test3()
+
+      b1 = new C();
+      b1.Test3(); //output=> C::Test3()
+
+
+
+      Console.ReadKey();
+    }
+  }
+}
